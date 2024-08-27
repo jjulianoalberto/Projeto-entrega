fccfbcf9b1ec07157870ebd2d5fa85af620b8bad
Juliano <jjulianoalberto@gmail.com>
2024-08-27 19:37:14 -0300

    Projeto Growflix

---
+++ src/assets/js/data.js
@@ -0,0 +1,15 @@
+const movies = [
+  {
+    img: 'https://img.youtube.com/vi/aJ-HZuLcKeA/sddefault.jpg',
+    title: 'Growcast #00 - Conheça a Growdev',
+    link: 'https://www.youtube.com/embed/aJ-HZuLcKeA?si=8mYIpe6U_iZJoHej',
+    category: 'growcast'
+  },
+  {
+    img: '',
+    title: '',
+    link: '',
+    category: ''
+  }
+]
+
